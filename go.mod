6d90090dc531d54acd36d15bdd0a6e374eaf717a
DustiasTheGuy <isakwebdevelopment@gmail.com>
2020-10-06 17:03:00 +0200

    added some stuff

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gorest
 go 1.15
 
 require (
-	github.com/gofiber/fiber/v2 v2.0.6 // indirect
-	github.com/gofiber/template v1.6.3 // indirect
+	github.com/gofiber/fiber/v2 v2.0.6
+	github.com/gofiber/redirect/v2 v2.0.0
+	github.com/gofiber/template v1.6.3
 )
--- main.go
+++ main.go
@@ -2,25 +2,50 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/html"
+	"github.com/gofiber/template/handlebars"
 )
 
-func index(c *fiber.Ctx) error {
-
-	return c.Render("index", fiber.Map{
-		"title": "Test",
-	})
-}
-
 func main() {
-	//engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{
-		Views: html.New("./views", ".html"),
+		Views: handlebars.New("./views", ".hbs"),
 	})
 
+	// prefix static folder with /public
 	app.Static("/public", "./public")
 
-	app.Get("/", index)
-
+	// string, handler
+	app.Get("/", CreateHandler)
+	app.Get("/create", CreateHandler)
+	app.Get("/read", ReadHandler)
+	app.Get("/update", UpdateHandler)
+	app.Get("/delete", DeleteHandler)
 	app.Listen(":3000")
+}
+
+func CreateHandler(c *fiber.Ctx) error {
+	// Create
+	return c.Render("create", fiber.Map{
+		"title": "Create", // Arbitrary
+	}, "layouts/main") // Extend the layouts.hbs file
+}
+
+func ReadHandler(c *fiber.Ctx) error {
+	// Read
+	return c.Render("read", fiber.Map{
+		"title": "Read", // Arbitrary
+	}, "layouts/main") // Extend the layouts.hbs file
+}
+
+func UpdateHandler(c *fiber.Ctx) error {
+	// Update
+	return c.Render("update", fiber.Map{
+		"title": "Update", // Arbitrary
+	}, "layouts/main") // Extend the layouts.hbs file
+}
+
+func DeleteHandler(c *fiber.Ctx) error {
+	// Delete
+	return c.Render("delete", fiber.Map{
+		"title": "Delete", // Arbitrary
+	}, "layouts/main") // Extend the layouts.hbs file
 }
